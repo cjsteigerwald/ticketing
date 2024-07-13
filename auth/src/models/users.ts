e586625246e0f6d8e47bf902f9aea9293cd5769d
Christopher Steigerwald <cjsteigerwald@gmail.com>
2024-07-13 04:46:32 -0700

    Creating static method to add User with TypeScript interface

--- auth/src/models/users.ts
+++ auth/src/models/users.ts
@@ -3,25 +3,26 @@ import mongoose, { mongo } from 'mongoose';
 // An interface that describes the properties
 // that are required to create a new User
 interface UserAttrs {
-  email: string;
-  password: string;
+	email: string;
+	password: string;
 }
 
 const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
+	email: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
 });
 
-const User = mongoose.model('User', userSchema);
-
-const buildUser = (attrs: UserAttrs) => {
-  return new User(attrs);
+// static interface to create User with TypeScript
+userSchema.statics.build = (attrs: UserAttrs) => {
+	return new User(attrs);
 };
+
+const User = mongoose.model('User', userSchema);
 
 export { User };
